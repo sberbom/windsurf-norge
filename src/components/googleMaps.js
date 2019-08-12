1d088b5504a83a6840a62980fd1b9cb8e18fce4b
Sigmund Berbom <sberbom@gmail.com>
2019-08-12 21:04:13 +0200

    rkey

--- src/components/googleMaps.js
+++ src/components/googleMaps.js
@@ -5,7 +5,7 @@ import Card from './card'
 
 const GoogleMaps = compose(
     withProps({
-      googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA6L6V97NXcF0Nr_RrYOaIntWEnZywcUrA&v=3.exp&libraries=geometry,drawing,places",
+      googleMapURL: "https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places",
       loadingElement: <div style={{ height: `100%` }} />,
       containerElement: <div style={{ height: `80vh` }} />,
       mapElement: <div style={{ height: `100%` }} />,
