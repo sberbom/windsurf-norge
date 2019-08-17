0d77da1b6897505368a5c1d07d38cd797cf9d753
Sigmund Berbom <sberbom@gmail.com>
2019-08-17 18:03:33 +0200

    Added posibility to add spot to database

--- src/containers/addSpotForm.js
+++ src/containers/addSpotForm.js
@@ -1,10 +1,20 @@
 import AddSpotForm from '../components/addSpotForm';
 import { connect } from 'react-redux';
+import {requestSpots} from '../state/actions';
     
 function mapStateToProps(state) {
     const props = {}
     props.address = state.getAddress.address;
+    props.lat = state.getAddress.lat;
+    props.lng = state.getAddress.lng;
+    props.spots = state.requestSpots.spots;
     return props;
 }
 
-export default connect(mapStateToProps, null)(AddSpotForm);
+const mapDispatchToProps = (dispatch) => {
+    return {
+      onRequestSpots: () => dispatch(requestSpots()),
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddSpotForm);
